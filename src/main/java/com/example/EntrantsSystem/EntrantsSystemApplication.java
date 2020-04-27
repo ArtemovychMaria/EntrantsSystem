d4a2555183729e69af83cd47350ddc0d9b750c98
Artemovych Maria <mari4ka.art.bloody@gmail.com>
2020-04-27 13:51:07 +0300

    init project

---
+++ src/main/java/com/example/EntrantsSystem/EntrantsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.EntrantsSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrantsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrantsSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EntrantsSystem/EntrantsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EntrantsSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrantsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
